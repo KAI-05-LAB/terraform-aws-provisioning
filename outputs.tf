4946785d5329788bf1e4a06eff2994ec23a108ca
Pratik Prakash <pratikai0555@gmail.com>
2026-07-12 12:31:55 +0530

    Add Terraform outputs

---
+++ outputs.tf
@@ -0,0 +1,15 @@
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "subnet_id" {
+  value = aws_subnet.public.id
+}
+
+output "security_group_id" {
+  value = aws_security_group.web_sg.id
+}
+
+output "bucket_name" {
+  value = aws_s3_bucket.storage.bucket
+}
